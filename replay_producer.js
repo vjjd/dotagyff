7a507fb3895eafe2ce9ac10962cd04f3350c6008
vjjd <bibikabum@gmail.com>
2016-10-21 04:49:00 +0600

    Init commit. Steam login: added; Replay producer: added.

---
+++ replay_producer.js
@@ -0,0 +1,81 @@
+'use strict';
+
+// Dependencies
+let Dota2 = require('./steam_login'),
+    request = require('request'),
+    Bunzip = require('seek-bzip'),
+    config = require('./config'),
+    fs = require('fs');
+
+// Variables
+let matchMeta = {
+    "id": 2715475517,
+    "heroName": "Vengeful Spirit",
+    "info": ""
+};
+
+Dota2.on("ready", onD2Ready);
+
+/**
+ * Dota2 Client on Ready
+ */
+function onD2Ready() {
+    console.log("Dota2 is Ready");
+
+    Dota2.requestMatchDetails(matchMeta.id);
+    Dota2.on("matchDetailsData", (matchId, matchData)=> {
+        matchMeta.info = matchData;
+        fetchReplay(matchMeta.id, matchMeta.info)
+            .then(decompressBZ2, onError)
+            .then(()=> {
+                console.log(`Done`);
+            }, onError);
+    });
+}
+
+/**
+ * Fetch Replay Data for Clip Recoding
+ */
+function fetchReplay(matchID, matchData) {
+    return new Promise((resolve, reject)=> {
+        let reqStream = request
+            .get(`http://replay${matchData.match.cluster}.valve.net/570/${matchID}_${matchData.match.replay_salt}.dem.bz2?v=1`)
+            .on('error', (err)=>{
+                if(err){ reject(err) }
+            })
+            .on('response', (res)=> {
+                if(res.statusCode != 200){ reject(`Status Code: ${res.statusCode}`) }
+            })
+            .pipe(fs.createWriteStream(`${config.bz2}/${matchID}.dem.bz2`));
+
+        reqStream.on('finish', ()=>{
+            console.log(`Replay #${matchID} was Downloaded`);
+            resolve(matchID);
+        })
+    })
+}
+
+/**
+ * Decomress dem.bz2 files
+ * @param matchID
+ * @returns {Promise}
+ */
+function decompressBZ2(matchID) {
+    return new Promise((resolve, reject)=> {
+        fs.readFile(`${config.bz2}/${matchID}.dem.bz2`, (err, compressedData)=> {
+            if(err){ reject(err) }
+            let data = Bunzip.decode(compressedData);
+
+            fs.writeFile(`${config.dem}/${matchID}.dem`, data, (err)=> {
+                if(err){ reject(err) }
+                console.log(`${matchID}.dem.bz2 was Decompressed`);
+
+                resolve();
+            });
+        });
+    })
+}
+
+function onError(err) {
+    console.log(err);
+}
---
+++ steam_login.js
@@ -0,0 +1,68 @@
+'use strict';
+
+let steam = require("steam"),
+    util = require("util"),
+    fs = require("fs"),
+    crypto = require("crypto"),
+    dota2 = require("dota2"),
+    steamClient = new steam.SteamClient(),
+    steamUser = new steam.SteamUser(steamClient),
+    Dota2 = new dota2.Dota2Client(steamClient, true);
+
+global.config = require("./config").steam;
+
+/**
+ * Steam Login
+ * @param logonResp
+ */
+let onSteamLogOn = function onSteamLogOn(logonResp) {
+    if (logonResp.eresult == steam.EResult.OK) {
+        util.log("Logged on.");
+        Dota2.launch();
+    }
+},
+    onSteamServers = function onSteamServers(servers) {
+        util.log("Received servers.");
+        fs.writeFile('servers', JSON.stringify(servers));
+    },
+    onSteamLogOff = function onSteamLogOff(eresult) {
+        util.log("Logged off from Steam.");
+    },
+    onSteamError = function onSteamError(error) {
+        util.log("Connection closed by server.");
+    };
+
+steamUser.on('updateMachineAuth', function(sentry, callback) {
+    let hashedSentry = crypto.createHash('sha1').update(sentry.bytes).digest();
+    fs.writeFileSync('sentry', hashedSentry);
+    util.log("sentryfile saved");
+
+    callback({ sha_file: hashedSentry});
+});
+
+let logOnDetails = {
+    "account_name": global.config.steam_user,
+    "password": global.config.steam_pass,
+};
+if (global.config.steam_guard_code) logOnDetails.auth_code = global.config.steam_guard_code;
+
+try {
+    let sentry = fs.readFileSync('sentry');
+    if (sentry.length) logOnDetails.sha_sentryfile = sentry;
+}
+catch (beef){
+    util.log("Cannot load the sentry. " + beef);
+}
+
+steamClient.connect();
+
+steamClient.on('connected', function() {
+    steamUser.logOn(logOnDetails);
+});
+
+steamClient.on('logOnResponse', onSteamLogOn);
+steamClient.on('loggedOff', onSteamLogOff);
+steamClient.on('error', onSteamError);
+steamClient.on('servers', onSteamServers);
+
+module.exports = Dota2;
