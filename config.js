459a6ac88b70e3c6596d78f78003d4239b109b0a
vjjd <bibikabum@gmail.com>
2016-11-01 21:43:34 +0700

    Frames dir Removal: added

--- config.js
+++ config.js
@@ -5,6 +5,14 @@
     "specMode" : 3,
     "recordFPS": 60
   },
+
+  "redis": {
+    "host": "localhost",
+    "port": 6379,
+    "auth": false,
+    "prefix": "dotagyff:"
+  },
+
   "bz2": "./tmp/bz2",
   "dem": "./tmp/dem",
   "dotaLogFile": "condump000.txt"
